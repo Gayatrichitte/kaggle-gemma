d343d2125d14ca0aaffeb1b04043d41bc22665fb
epicxxvivek <vivekkatore1110@gmail.com>
2026-08-02 16:11:38 +0530

    fix Leaderboard to connect live to Firebase Firestore database and remove all hardcoded mock data

--- html-version/firebase-config.js
+++ html-version/firebase-config.js
@@ -22,6 +22,7 @@ import {
   where,
   orderBy,
   limit,
+  onSnapshot,
   serverTimestamp 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
@@ -73,5 +74,6 @@ export {
   where,
   orderBy,
   limit,
+  onSnapshot,
   serverTimestamp
 };
